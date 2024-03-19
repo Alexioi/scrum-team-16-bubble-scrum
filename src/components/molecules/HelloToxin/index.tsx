38dc2bfd71728397126270a6a8460273c3bcef18
Ivanov Alexey <ivanovdev@disroot.org>
2024-03-19 20:57:35 +0300

    add pretter config

--- src/components/molecules/HelloToxin/index.tsx
+++ src/components/molecules/HelloToxin/index.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { RootState, actions } from "@/store";
-import { useAppDispatch, useAppSelector } from "@/hooks";
+import { RootState, actions } from '@/store';
+import { useAppDispatch, useAppSelector } from '@/hooks';
 
-import style from "./style.module.scss";
+import style from './style.module.scss';
 
 const HelloToxin = () => {
   const dispatch = useAppDispatch();
   const helloToxin = useAppSelector((state: RootState) => state.helloToxin);
 
   const onClickButton = () => {
-    dispatch(actions.helloToxin.change("Hello, Toxin!"));
+    dispatch(actions.helloToxin.change('Hello, Toxin!'));
   };
 
   return (
