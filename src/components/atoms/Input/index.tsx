8acf7ecd417015634d8fcd42c2d13daa152c3070
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-21 12:04:34 +0400

    resctructure Input component with linter

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,20 +1,28 @@
-"use client";
+/* eslint-disable react/require-default-props */
 
-import React, { InputHTMLAttributes } from "react";
-import style from "./style.module.scss";
+'use client';
+
+import React, { InputHTMLAttributes } from 'react';
+import style from './style.module.scss';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-	type?: "search" | "text" | "email" | "password";
+  type?: 'text' | 'email' | 'password' | 'date';
 }
 
-const Input: React.FC<Props> = ({ type = "text", ...standardProps }) => {
-	return (
-		<input
-			className={style.input}
-			type={type}
-			{...standardProps}
-		/>
-	);
+const Input: React.FC<Props> = ({ type = 'text', ...standardProps }) => {
+  return (
+    <input
+      className={style.input}
+      type={type}
+      name={standardProps.name}
+      id={standardProps.id}
+      value={standardProps.value}
+      min={standardProps.min}
+      max={standardProps.max}
+      placeholder={standardProps.placeholder}
+      readOnly={standardProps.readOnly}
+    />
+  );
 };
 
 export default Input;
