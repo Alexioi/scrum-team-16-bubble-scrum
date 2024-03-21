8dfaa403faf737b5ce551d38630abcd21ef0aeda
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-21 12:01:46 +0400

    display input variations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import {HelloToxin, Input } from '@/components';
+import { HelloToxin, Input } from '@/components';
 
 const Home = () => {
-  return <>
-    <HelloToxin />
-    <Input placeholder="Email" />
-    <input type="email" />
-    </>;
+  return (
+    <>
+      <HelloToxin />
+      <div>
+        <h2>Date:</h2>
+        <Input type="date" min="2024-03-20" />
+        <h2>Email:</h2>
+        <Input type="email" placeholder="Email" />
+        <h2>Text:</h2>
+        <Input type="text" placeholder="Текст" value="This is pretty awesome" />
+        <h2>Password:</h2>
+        <Input type="password" placeholder="Пароль" />
+      </div>
+    </>
+  );
 };
 
 export default Home;
