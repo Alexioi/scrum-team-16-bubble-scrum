ca29487d05fdecf27d855fc370e4109700484544
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-21 12:41:01 +0400

    format output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ const Home = () => {
     <>
       <HelloToxin />
       <div>
-        <h2>Date:</h2>
         <Input type="date" min="2024-03-20" />
-        <h2>Email:</h2>
         <Input type="email" placeholder="Email" />
-        <h2>Text:</h2>
         <Input type="text" placeholder="Текст" value="This is pretty awesome" />
-        <h2>Password:</h2>
         <Input type="password" placeholder="Пароль" />
+        <Input
+          type="text"
+          placeholder="Текст"
+          value="This is pretty readonly"
+          readOnly
+        />
       </div>
     </>
   );
