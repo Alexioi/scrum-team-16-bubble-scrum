292194dfe72ecdc00a510ec0a11dd8e34c9ecb99
Ivanov Alexey <ivanovdev@disroot.org>
2024-03-23 19:51:35 +0300

    add clear input values

--- src/components/molecules/Calendar/index.tsx
+++ src/components/molecules/Calendar/index.tsx
@@ -44,6 +44,8 @@ const Calendar: FC<Props> = ({ isSingle }) => {
   };
 
   const handleClearButtonClick = () => {
+    setFirstInputValue('');
+    setSecondInputValue('');
     setCalendarValue(null);
   };
 
