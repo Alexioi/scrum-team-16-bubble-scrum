c0ad7511048cdfb180a6ac0d9fac023e005118d2
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-25 14:59:53 +0400

    interface -> type, remove default value and style property

--- src/components/atoms/Typography/index.tsx
+++ src/components/atoms/Typography/index.tsx
@@ -1,43 +1,22 @@
 'use client';
 
-import { CSSProperties, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import styles from './style.module.scss';
 
 type TagVariants = 'h1' | 'h2' | 'h3' | 'p' | 'span';
 
-interface Props {
+type Props = {
   children: ReactNode;
-  tag?: TagVariants;
-  style?: CSSProperties | undefined;
-}
+  tag: TagVariants;
+};
 
-const Typography: React.FC<Props> = ({ children, tag = 'p', style }) => {
+const Typography: React.FC<Props> = ({ children, tag }) => {
   return {
-    p: (
-      <p style={style} className={styles.typography__p}>
-        {children}
-      </p>
-    ),
-    h1: (
-      <h1 style={style} className={styles.typography__h1}>
-        {children}
-      </h1>
-    ),
-    h2: (
-      <h2 style={style} className={styles.typography__h2}>
-        {children}
-      </h2>
-    ),
-    h3: (
-      <h3 style={style} className={styles.typography__h3}>
-        {children}
-      </h3>
-    ),
-    span: (
-      <span style={style} className={styles.typography}>
-        {children}
-      </span>
-    ),
+    p: <p className={styles.typography__p}>{children}</p>,
+    h1: <h1 className={styles.typography__h1}>{children}</h1>,
+    h2: <h2 className={styles.typography__h2}>{children}</h2>,
+    h3: <h3 className={styles.typography__h3}>{children}</h3>,
+    span: <span className={styles.typography}>{children}</span>,
   }[tag];
 };
 
