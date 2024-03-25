82e7df24db16efb8ed7cda696e3f9ca21b79e3e4
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-25 15:04:02 +0400

    rename classes

--- src/components/atoms/Typography/index.tsx
+++ src/components/atoms/Typography/index.tsx
@@ -12,11 +12,11 @@ type Props = {
 
 const Typography: React.FC<Props> = ({ children, tag }) => {
   return {
-    p: <p className={styles.typography__p}>{children}</p>,
-    h1: <h1 className={styles.typography__h1}>{children}</h1>,
-    h2: <h2 className={styles.typography__h2}>{children}</h2>,
-    h3: <h3 className={styles.typography__h3}>{children}</h3>,
-    span: <span className={styles.typography}>{children}</span>,
+    p: <p className={styles.p}>{children}</p>,
+    h1: <h1 className={styles.h1}>{children}</h1>,
+    h2: <h2 className={styles.h2}>{children}</h2>,
+    h3: <h3 className={styles.h3}>{children}</h3>,
+    span: <span className={styles.span}>{children}</span>,
   }[tag];
 };
 
