522cd60e9a8e373c95e6aa850f632a76e47ab4ac
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-03-25 18:53:04 +0700

    7 add images

--- src/components/atoms/Rating/index.tsx
+++ src/components/atoms/Rating/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
-import { FillStar } from '../icons';
-import { OutlineStar } from '../icons';
+
+import FillStar from '@/images/decorative/star.svg';
+import OutlineStar from '@/images/decorative/outline-star.svg';
 
 import style from './style.module.scss';
 
@@ -11,13 +12,29 @@ type Props = {
 const Rating: FC<Props> = ({ rating }) => {
   return (
     <div className={style.rating}>
-      {[1, 2, 3, 4, 5].map((number) =>
-        number <= rating ? (
-          <FillStar key={number} />
+      {[1, 2, 3, 4, 5].map((number) => {
+        return number <= rating ? (
+          <svg width="21" height="19" key={number} fill={`url('#gradient')`}>
+            <FillStar />
+            <defs>
+              <linearGradient id="gradient">
+                <stop stopColor="#BC9CFF" />
+                <stop offset="1" stopColor="#8BA4F9" />
+              </linearGradient>
+            </defs>
+          </svg>
         ) : (
-          <OutlineStar key={number} />
-        ),
-      )}
+          <svg width="21" height="19" key={number} fill="url('#gradient')">
+            <OutlineStar />
+            <defs>
+              <linearGradient id="gradient">
+                <stop stopColor="#BC9CFF" />
+                <stop offset="1" stopColor="#8BA4F9" />
+              </linearGradient>
+            </defs>
+          </svg>
+        );
+      })}
     </div>
   );
 };
--- src/store/HotelListSlice/mockHotels.ts
+++ src/store/HotelListSlice/mockHotels.ts
@@ -1,53 +1,56 @@
-import { Hotel } from "@/components";
-import img from "@/assets/hotel/image.jpg";
+import { Hotel } from '@/components';
+import img1 from '@/images/content/room-image-1.jpg';
+import img2 from '@/images/content/room-image-2.jpg';
+import img3 from '@/images/content/room-image-3.jpg';
+import img4 from '@/images/content/room-image-4.jpg';
 
 export const mockHotels: Hotel[] = [
-    {
-        id: 0,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: true,
-        number: "567",
-        price: 9100,
-        rating: 3,
-    },
-    {
-        id: 2,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: true,
-        number: "124",
-        price: 19100,
-        rating: 1,
-    },
-    {
-        id: 3,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: false,
-        number: "21",
-        price: 910,
-        rating: 2,
-    },
-    {
-        id: 4,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: false,
-        number: "1",
-        price: 2700,
-        rating: 0,
-    },
-    {
-        id: 5,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: false,
-        number: "1234",
-        price: 5500,
-        rating: 4,
-    },
-    {
-        id: 6,
-        images: [img.src, img.src, img.src, img.src],
-        isLuxury: false,
-        number: "123",
-        price: 5500,
-        rating: 5,
-    },
-]
+  {
+    id: 0,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: true,
+    number: '567',
+    price: 9100,
+    rating: 3,
+  },
+  {
+    id: 2,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: true,
+    number: '124',
+    price: 19100,
+    rating: 1,
+  },
+  {
+    id: 3,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: false,
+    number: '21',
+    price: 910,
+    rating: 2,
+  },
+  {
+    id: 4,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: false,
+    number: '1',
+    price: 2700,
+    rating: 0,
+  },
+  {
+    id: 5,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: false,
+    number: '1234',
+    price: 5500,
+    rating: 4,
+  },
+  {
+    id: 6,
+    images: [img1.src, img2.src, img3.src, img4.src],
+    isLuxury: false,
+    number: '123',
+    price: 5500,
+    rating: 5,
+  },
+];
