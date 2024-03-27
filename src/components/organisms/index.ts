a632827adfd52ba98863ef9d24bb1087728d9a2f
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-03-27 14:11:11 +0400

    refactor exports

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,6 +2,4 @@ import { Header } from './Header';
 import { SubscriptionInput } from './SubscriptionInput';
 import { DropdownInput } from './DropdownInput';
 
-export { Header };
-
-export { SubscriptionInput, DropdownInput };
+export { Header, SubscriptionInput, DropdownInput };
