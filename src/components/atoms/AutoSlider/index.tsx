91d7b6169d4e8356953908904ffba2b3bc331a07
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-03-29 15:59:23 +0700

    add map for slides

--- src/components/atoms/AutoSlider/index.tsx
+++ src/components/atoms/AutoSlider/index.tsx
@@ -1,17 +1,25 @@
+import { FC } from 'react';
 import Image from 'next/image';
-
-import bg1 from '@/images/content/landing-page-background.jpg';
-import bg2 from '@/images/content/landing-page-background2.jpg';
-import bg3 from '@/images/content/landing-page-background3.jpg';
 
 import style from './style.module.scss';
 
-const AutoSlider = () => {
+type Props = {
+  imagesURLs: string[];
+};
+
+const AutoSlider: FC<Props> = ({ imagesURLs }) => {
   return (
     <div className={style.slider}>
-      <Image className={style.slide} fill src={bg1} alt="Номер отеля" />
-      <Image className={style.slide} fill src={bg2} alt="Номер отеля" />
-      <Image className={style.slide} fill src={bg3} alt="Номер отеля" />
+      {imagesURLs.map((url) => (
+        <Image
+          key={url}
+          className={style.slide}
+          fill
+          src={url}
+          alt="Номер отеля"
+        />
+      ))}
+
       <div className={style.desc}>
         <div className={style.text}>
           Лучшие номера для вашей работы, отдыха и просто вдохновения
--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,11 +1,12 @@
 import { AutoSlider } from '@/components/atoms';
 
 import style from './style.module.scss';
+import { images } from './data';
 
 const Hero = () => {
   return (
     <div className={style.hero}>
-      <AutoSlider />
+      <AutoSlider imagesURLs={images} />
     </div>
   );
 };
