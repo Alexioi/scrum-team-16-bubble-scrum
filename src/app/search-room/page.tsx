38ac25b872c4471e341cac17ee24a3335c018ddf
Ivanov Alexey <ivanovdev@disroot.org>
2024-03-29 11:55:15 +0300

    add scelet for search room

--- src/app/search-room/page.tsx
+++ src/app/search-room/page.tsx
@@ -1,10 +1,25 @@
-import { Filter, Container } from '@/components';
+import { Filter, Container, Typography } from '@/components';
+
+import style from './style.module.scss';
 
 const SearchRoom = () => {
   return (
-    <Container>
-      <Filter />
-    </Container>
+    <div className={style['search-room']}>
+      <Container>
+        <div className={style.content}>
+          <div className={style['filter-wrapper']}>
+            <Filter />
+          </div>
+          <div className={style.column}>
+            <Typography tag="h2">
+              Номера, которые мы для вас подобрали
+            </Typography>
+            <div className={style['room-list-wrapper']}>room-list</div>
+            <div className={style.pagination}>pagination</div>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
