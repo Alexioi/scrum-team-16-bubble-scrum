04e2b23818c2bd81f4d6dcb2b73f6d83f8459e7b
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-03-29 19:31:06 +0700

    add hero with search card

--- src/components/organisms/Hero/data.ts
+++ src/components/organisms/Hero/data.ts
@@ -4,4 +4,21 @@ import bg3 from '@/images/content/landing-page-background3.jpg';
 
 const images: string[] = [bg1.src, bg2.src, bg3.src];
 
-export { images };
+const guestVariants = [
+  ['взрослый', 'взрослых', 'взрослых'],
+  ['ребенок', 'ребенка', 'детей'],
+  ['младенец', 'младенца', 'младенцев'],
+];
+
+const guestItems = [
+  {
+    name: 'Взрослые',
+    counter: 0,
+  },
+  { name: 'Дети', counter: 0 },
+  { name: 'Младенцы', counter: 0 },
+];
+
+const guestGroups = [[0], [1], [2]];
+
+export { images, guestVariants, guestItems, guestGroups };
--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -1,11 +1,52 @@
-import { AutoSlider } from '@/components/atoms';
+'use client';
+
+import { AutoSlider, Button, Container, Typography } from '@/components/atoms';
+import { Dropdown } from '@/components/molecules';
 
 import style from './style.module.scss';
-import { images } from './data';
+import { guestGroups, guestItems, guestVariants, images } from './data';
+import { Calendar } from '../Calendar';
 
 const Hero = () => {
   return (
     <div className={style.hero}>
+      <Container>
+        <div className={style['search-wrapper']}>
+          <Typography tag="h1">Найдём номера под ваши пожелания</Typography>
+
+          <div className={style['calendar-wrapper']}>
+            <div className={style.titles}>
+              <Typography tag="h3">Прибытие</Typography>
+              <Typography tag="h3">Выезд</Typography>
+            </div>
+            <Calendar />
+          </div>
+
+          <div className={style['guest-wrapper']}>
+            <div className={style.title}>
+              <Typography tag="h3">Гости</Typography>
+            </div>
+            <Dropdown
+              hasButtons
+              placeholder="Сколько гостей"
+              groups={guestGroups}
+              variants={guestVariants}
+              items={guestItems}
+            />
+          </div>
+
+          <div className={style['button-wrapper']}>
+            <Button text="Подобрать номер" theme="long" onClick={() => {}} />
+          </div>
+        </div>
+      </Container>
+      <div className={style.desc}>
+        <Container>
+          <div className={style.text}>
+            Лучшие номера для вашей работы, отдыха и просто вдохновения
+          </div>
+        </Container>
+      </div>
       <AutoSlider imagesURLs={images} />
     </div>
   );
