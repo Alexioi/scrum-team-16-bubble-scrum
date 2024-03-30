b2db8b567fd867dcbf1f7b34f9b160a0aad3253c
Ivanov Alexey <ivanovdev@disroot.org>
2024-03-30 16:29:20 +0300

    disable ssr

--- src/libs/ReactCalendar/index.tsx
+++ src/libs/ReactCalendar/index.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import { FC, Dispatch, SetStateAction } from 'react';
-import Calendar from 'react-calendar';
+import dynamic from 'next/dynamic';
 
 import ArrowSVG from '@/images/decorative/arrow.svg';
 
 import './style.scss';
+
+const Calendar = dynamic(() => import('react-calendar'), { ssr: false });
 
 type CalendarValue = Date | null | [Date | null, Date | null];
 
@@ -26,7 +28,6 @@ const ReactCalendar: FC<Props> = ({ value, onChange }) => {
       }}
       onChange={onChange}
       value={value}
-      locale="ru-RU"
       selectRange
       minDate={new Date()}
       minDetail="month"
