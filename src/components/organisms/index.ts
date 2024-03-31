9cdccccdf9d3575d85840dce5f8371c58fbfb1c1
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2024-03-31 21:22:41 +0300

    makes fixes for Pagination import

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -4,6 +4,14 @@ import { Footer } from './Footer';
 import { HotelList } from './HotelList';
 import { Hotel } from './HotelCard';
 import { Header } from './Header';
+import { Pagination } from './Pagination';
 
-export { SubscriptionInput, DropdownInput, Footer, Header, HotelList };
+export {
+  SubscriptionInput,
+  DropdownInput,
+  Footer,
+  Header,
+  HotelList,
+  Pagination,
+};
 export type { Hotel };
