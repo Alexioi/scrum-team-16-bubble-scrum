200afad77a881a82ec49a832d7923f56f2733cf4
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-02 15:50:14 +0300

    succes set firestore

--- src/components/organisms/HotelList/index.tsx
+++ src/components/organisms/HotelList/index.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, child, get } from 'firebase/database';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 import { getAllHotels } from '@/store/HotelListSlice';
 import { useAppDispatch, useAppSelector } from '@/hooks';
@@ -10,40 +10,34 @@ import { useAppDispatch, useAppSelector } from '@/hooks';
 import { HotelCard } from '../HotelCard';
 import style from './style.module.scss';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBka4nZbcAKM_if00V5UwDCmNSWAmldueY',
-  authDomain: 'bubble-scrum.firebaseapp.com',
-  databaseURL: 'https://bubble-scrum.europe-west1.firebaseio.com',
-  projectId: 'bubble-scrum',
-  storageBucket: 'bubble-scrum.appspot.com',
-  messagingSenderId: '525026535335',
-  appId: '1:525026535335:web:cb05321bfd1b41f9107882',
-  measurementId: 'G-1Y4V34D1YP',
-};
-
-const app = initializeApp(firebaseConfig);
-
 const HotelList = () => {
   const { data } = useAppSelector((state) => state.hotelList);
   const dispatch = useAppDispatch();
 
-  const dbRef = ref(getDatabase(app));
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBka4nZbcAKM_if00V5UwDCmNSWAmldueY',
+      authDomain: 'bubble-scrum.firebaseapp.com',
+      projectId: 'bubble-scrum',
+      storageBucket: 'bubble-scrum.appspot.com',
+      messagingSenderId: '525026535335',
+      appId: '1:525026535335:web:cb05321bfd1b41f9107882',
+      measurementId: 'G-1Y4V34D1YP',
+    };
 
-  const x = async () => {
-    await get(child(dbRef, 'test'))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log(snapshot.val());
-        } else {
-          console.log('No data available');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
-  x();
+    async function getCities() {
+      const citiesCol = collection(db, 'test');
+      const citySnapshot = await getDocs(citiesCol);
+      const cityList = citySnapshot.docs.map((doc) => doc.data());
+      console.log(cityList);
+      return cityList;
+    }
+
+    getCities();
+  }, []);
 
   useEffect(() => {
     dispatch(getAllHotels());
