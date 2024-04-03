912f36ad6fe6126bb71c344d03b3287c560f73ca
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2024-04-03 20:06:00 +0300

    adds Checkbox export

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,5 +2,6 @@ import { HelloToxin } from './HelloToxin';
 import { Hero } from './Hero';
 import { ButtonInput } from './ButtonInput';
 import { Dropdown } from './Dropdown';
+import { Checkbox } from './Checkbox';
 
-export { HelloToxin, ButtonInput, Hero, Dropdown };
+export { HelloToxin, ButtonInput, Hero, Dropdown, Checkbox };
