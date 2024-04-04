9e10777176ddc3b91d18374e5f481f76df267d77
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-04-04 18:33:20 +0400

    remove useEffects

--- src/components/molecules/MultiRangeSlider/index.tsx
+++ src/components/molecules/MultiRangeSlider/index.tsx
@@ -1,13 +1,6 @@
 'use client';
 
-import {
-  ChangeEvent,
-  FC,
-  useCallback,
-  useEffect,
-  useState,
-  useRef,
-} from 'react';
+import { ChangeEvent, FC, useCallback, useState, useRef } from 'react';
 import clsx from 'clsx';
 
 import style from './style.module.scss';
@@ -35,31 +28,18 @@ const MultiRangeSlider: FC<Props> = ({ min, max, onChange }) => {
     [min, max],
   );
 
-  useEffect(() => {
-    if (!maxValueRef.current || !range.current) {
-      return;
-    }
-    const minPercent = getPercent(minValue);
-    const maxPercent = getPercent(Number(maxValueRef.current.value));
-
-    range.current.style.left = `${minPercent}%`;
-    range.current.style.width = `${maxPercent - minPercent}%`;
-  }, [minValue, getPercent]);
-
-  useEffect(() => {
-    if (!minValueRef.current || !range.current) {
-      return;
-    }
-    const minPercent = getPercent(Number(minValueRef.current.value));
-    const maxPercent = getPercent(maxValue);
-
-    range.current.style.width = `${maxPercent - minPercent}%`;
-  }, [maxValue, getPercent]);
-
   const onChangeMin = (event: ChangeEvent<HTMLInputElement>): void => {
     const value = Math.min(Number(event.target.value), maxValue - 1);
     setMinValue(value);
     event.target.value = value.toString();
+
+    if (maxValueRef.current && range.current) {
+      const minPercent = getPercent(minValue);
+      const maxPercent = getPercent(Number(maxValueRef.current.value));
+
+      range.current.style.left = `${minPercent}%`;
+      range.current.style.width = `${maxPercent - minPercent}%`;
+    }
 
     onChange({ minValue, maxValue });
   };
@@ -68,6 +48,13 @@ const MultiRangeSlider: FC<Props> = ({ min, max, onChange }) => {
     const value = Math.max(Number(event.target.value), minValue + 1);
     setMaxValue(value);
     event.target.value = value.toString();
+
+    if (minValueRef.current && range.current) {
+      const minPercent = getPercent(Number(minValueRef.current.value));
+      const maxPercent = getPercent(maxValue);
+
+      range.current.style.width = `${maxPercent - minPercent}%`;
+    }
 
     onChange({ minValue, maxValue });
   };
