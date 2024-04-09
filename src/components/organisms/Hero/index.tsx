5d3ad7284119b4f5470c4f69028aa5721423dced
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-04-09 15:56:08 +0700

    add state for main page

--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -1,20 +1,27 @@
 'use client';
 
+import Link from 'next/link';
+
 import { AutoSlider, Button, Container, Typography } from '@/components/atoms';
 import { Dropdown, Calendar } from '@/components/organisms';
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { actions } from '@/store';
 
 import style from './style.module.scss';
 import { guestGroups, guestItems, guestVariants, images } from './data';
 
 const Hero = () => {
+  const dispatch = useAppDispatch();
+  const { guestsData } = useAppSelector((state) => state.filter);
+
   const handleDateCalendarChange = (value: string[] | null[]) => {
-    console.log(value);
+    dispatch(actions.filter.changeDates(value));
   };
 
   const handleGeustDropdownChange = (
     value: { name: string; counter: number }[],
   ) => {
-    console.log(value);
+    dispatch(actions.filter.changeGuestData(value));
   };
 
   return (
@@ -40,13 +47,15 @@ const Hero = () => {
               placeholder="Сколько гостей"
               groups={guestGroups}
               variants={guestVariants}
-              items={guestItems}
+              items={guestsData.items || guestItems}
               onChange={handleGeustDropdownChange}
             />
           </div>
 
           <div className={style['button-wrapper']}>
-            <Button text="Подобрать номер" theme="long" onClick={() => {}} />
+            <Link href="/search-room">
+              <Button text="Подобрать номер" theme="long" onClick={() => {}} />
+            </Link>
           </div>
         </div>
       </Container>
