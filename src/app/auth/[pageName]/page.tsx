3ae30e017f372b01685c43deddacb9736f2edfab
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-16 13:55:47 +0300

    change route to notFound

--- src/app/auth/[pageName]/page.tsx
+++ src/app/auth/[pageName]/page.tsx
@@ -1,31 +1,22 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { notFound } from 'next/navigation';
 
 import style from './style.module.scss';
 
 const Auth = ({ params }: { params: { pageName: string } }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!['login', 'registration'].includes(params.pageName)) {
-      router.push('/404');
-    }
-  }, [params.pageName, router]);
-
-  if (['login', 'registration'].includes(params.pageName)) {
-    return (
-      <div className={style.registration}>
-        <div className={style.form}>
-          {params.pageName === 'login' && 'login'}
-          {params.pageName === 'registration' && 'registration'}
-        </div>
-      </div>
-    );
+  if (!['login', 'registration'].includes(params.pageName)) {
+    notFound();
   }
 
-  return null;
+  return (
+    <div className={style.registration}>
+      <div className={style.form}>
+        {params.pageName === 'login' && 'login'}
+        {params.pageName === 'registration' && 'registration'}
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
