f8da960de5d4576cc959a0adc4ef18033bd901e9
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-17 16:05:04 +0300

    add sing up

--- src/components/organisms/SingUp/index.tsx
+++ src/components/organisms/SingUp/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, ChangeEvent } from 'react';
 
 import {
   Card,
@@ -11,25 +11,73 @@ import {
   RadioButtons,
   Toggle,
 } from '@/components';
+import { createNewUser } from '@/api';
 
 import style from './style.module.scss';
 
 const SingUp = () => {
-  const [toggleValues, setToggleValues] = useState(true);
+  const [toggleValue, setToggleValue] = useState(true);
   const [radioButtonsValues, setRadioButtonsValues] = useState([
     { value: 'man', text: 'мужчина', isChecked: true },
     { value: 'woman', text: 'женщина', isChecked: false },
   ]);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleNameInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.currentTarget.value);
+  };
+  const handleSurnameInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSurname(e.currentTarget.value);
+  };
+  const handleBirthdayInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBirthday(e.currentTarget.value);
+  };
+  const handleEmailInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const handlePasswordInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const handleSingUpButtonClick = () => {
+    createNewUser(
+      name,
+      surname,
+      radioButtonsValues.find((item) => {
+        return item.isChecked;
+      })?.value || 'man',
+      birthday,
+      email,
+      password,
+      toggleValue,
+    );
+  };
 
   return (
     <Card>
       <form>
         <Typography tag="h1">Регистрация аккаунта</Typography>
         <div className={style.name}>
-          <Input type="text" name="name" placeholder="Имя" />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Имя"
+            value={name}
+            onChange={handleNameInputChange}
+          />
         </div>
         <div className={style.surname}>
-          <Input type="text" name="surname" placeholder="Фамилия" />
+          <Input
+            type="text"
+            name="surname"
+            placeholder="Фамилия"
+            value={surname}
+            onChange={handleSurnameInputChange}
+          />
         </div>
         <div className={style['radio-buttons']}>
           <RadioButtons
@@ -42,7 +90,13 @@ const SingUp = () => {
           <Typography tag="h3">дата рождения</Typography>
         </div>
         <div className={style['birthday-input']}>
-          <Input type="text" name="birthday" placeholder="ДД.ММ.ГГГГ" />
+          <Input
+            type="text"
+            name="birthday"
+            placeholder="ДД.ММ.ГГГГ"
+            value={birthday}
+            onChange={handleBirthdayInputChange}
+          />
         </div>
         <div className={style.login}>
           <Typography tag="h3">данные для входа в сервис</Typography>
@@ -53,20 +107,32 @@ const SingUp = () => {
             inputMode="email"
             name="email"
             placeholder="Email"
+            value={email}
+            onChange={handleEmailInputChange}
           />
         </div>
         <div className={style.password}>
-          <Input type="password" name="password" placeholder="Пароль" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={handlePasswordInputChange}
+          />
         </div>
         <div className={style.toggle}>
           <Toggle
             text="Получать спецпредложения"
-            isChecked={toggleValues}
-            onClick={setToggleValues}
+            isChecked={toggleValue}
+            onClick={setToggleValue}
           />
         </div>
         <div className={style['submit-button']}>
-          <Button theme="long" text="войти" onClick={() => {}} />
+          <Button
+            theme="long"
+            text="зарегистрироваться"
+            onClick={handleSingUpButtonClick}
+          />
         </div>
       </form>
       <div className={style['question-about-auth']}>
