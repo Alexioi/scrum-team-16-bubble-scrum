5bd4d926d87d03b4b88ee0fd8c0ae9ccfc11f40a
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-18 11:37:32 +0300

    fix redirect when user login

--- src/app/auth/[pageName]/page.tsx
+++ src/app/auth/[pageName]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { notFound, useRouter } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 import { SingIn, SingUp } from '@/components';
 import { selectUID } from '@/store';
@@ -10,10 +10,9 @@ import style from './style.module.scss';
 
 const Auth = ({ params }: { params: { pageName: string } }) => {
   const uid = useAppSelector(selectUID);
-  const router = useRouter();
 
   if (uid !== '') {
-    router.push('/');
+    return redirect('/');
   }
 
   if (!['sign-in', 'sign-up'].includes(params.pageName)) {
