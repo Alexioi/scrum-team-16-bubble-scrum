43735da08bacaf413a092769b2a93905e572cdc7
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-24 11:51:17 +0300

    add features query

--- src/components/organisms/RoomFeatures/index.tsx
+++ src/components/organisms/RoomFeatures/index.tsx
@@ -1,16 +1,62 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState, useEffect } from 'react';
 
 import { RoomFeature, Typography } from '@/components';
+import { useAppSelector } from '@/hooks';
+import { selectRoom } from '@/store';
+import { getFeaturesByRoomId } from '@/api';
 
 import { title } from './constants';
 import style from './style.module.scss';
 
 const RoomFeatures: FC = () => {
-  const features = [
-    { name: 'smile', title: 'Комфорт', text: 'Шумопоглощающие стены' },
-    { name: 'house', title: 'Удобство', text: 'Окно в каждой из спален' },
-    { name: 'flame', title: 'Уют', text: 'Номер оснащён камином' },
-  ];
+  const room = useAppSelector(selectRoom);
+  const [features, setFeatures] = useState<
+    {
+      name: string;
+      title: string;
+      text: string;
+      userUid: string;
+      roomId: string;
+    }[]
+  >([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (room?.id === undefined) {
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        setError('');
+        setIsLoading(true);
+        const featuresData = await getFeaturesByRoomId(room.id);
+
+        setFeatures(featuresData);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+          return;
+        }
+        setError('неизвестная ошибка');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [room]);
+
+  if (isLoading) {
+    return 'Загрузка...';
+  }
+
+  if (error) {
+    return error;
+  }
 
   return (
     <div className={style.about}>
