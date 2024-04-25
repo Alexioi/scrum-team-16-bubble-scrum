e0d87cedf4436dbb6902300e44b6747a515300d2
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-04-25 12:30:23 +0400

    add resize behavior

--- src/components/organisms/ImageGallery/ImageGallery.tsx
+++ src/components/organisms/ImageGallery/ImageGallery.tsx
@@ -1,38 +1,47 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
 
+import useScreenSize from '@/hooks/useScreenSize';
+import { Swiper } from '@/components/molecules';
 import img1 from '@/images/content/gallery-main.jpg';
 import img2 from '@/images/content/gallery-sub-top.jpg';
 import img3 from '@/images/content/gallery-sub-bottom.jpg';
 
 import style from './style.module.scss';
+import { Placeholder } from './Placeholder';
 
-const testImages = [img1, img2, img3];
+const images = [img1, img2, img3];
 
 const ImageGallery = () => {
+  const windowSize = useScreenSize();
+
+  if (images.length === 0) {
+    return <Placeholder />;
+  }
+
+  if (windowSize[0] < 992) {
+    return <Swiper imageNames={images.map((item) => item.src)} />;
+  }
+
+  console.log(images[0].src);
+
   return (
     <div className={style.gallery}>
       <div>
-        <Image className={style.image} src={testImages[0]} alt="Room image" />
+        <Image className={style.image} src={images[0]} alt="Room image" />
       </div>
-      <div className={style.column}>
-        <div>
+      {images.length >= 3 && (
+        <div className={style.column}>
           <div>
-            <Image
-              className={style.image}
-              src={testImages[1]}
-              alt="Room image"
-            />
+            <Image className={style.image} src={images[1]} alt="Room image" />
           </div>
           <div>
-            <Image
-              className={style.image}
-              src={testImages[2]}
-              alt="Room image"
-            />
+            <Image className={style.image} src={images[2]} alt="Room image" />
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/organisms/ImageGallery/ImageGallerySkeleton.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ImageGallerySkeleton = () => {
-  return <div>ImageGallerySkeleton</div>;
-};
-
-export { ImageGallerySkeleton };
