369b5287ff2dfc18f76d0d0eb8c21e9fa96fb51a
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-25 09:55:39 +0300

    add elements to room page

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -1,25 +1,39 @@
 'use client';
 
-import { useState, FC } from 'react';
+import { FC } from 'react';
 
-import { LikeButton, BookingCard, GetRoomCardData } from '@/components';
+import {
+  BookingCard,
+  GetRoomCardData,
+  Container,
+  CommentList,
+} from '@/components';
+
+import style from './style.module.scss';
 
 type Props = { params: { id: string } };
 
 const RoomPage: FC<Props> = ({ params }) => {
-  const [isLiked, setIsLiked] = useState(false);
-
   return (
     <GetRoomCardData id={params.id}>
-      <LikeButton
-        countLikes={isLiked ? 23 : 22}
-        active={isLiked}
-        onClick={() => {
-          setIsLiked(!isLiked);
-        }}
-      />
-      <div style={{ width: '380px', margin: 'auto' }}>
-        <BookingCard />
+      <div className={style['room-details']}>
+        Галерея
+        <Container>
+          <div className={style['content-block']}>
+            <div className={style['content-column']}>
+              <div className={style['room-statistic']}>
+                <section className={style['icon-text-list']}>
+                  icon-text-list
+                </section>
+                <section className={style['pie-chart']}>pie-chart</section>
+              </div>
+              <CommentList />
+            </div>
+          </div>
+          <div className={style['room-booking']}>
+            <BookingCard />
+          </div>
+        </Container>
       </div>
     </GetRoomCardData>
   );
