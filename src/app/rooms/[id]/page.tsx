78405f804083a3324f7907fa5b989d2becd52ab1
Ivanov Alexey <ivanovdev@disroot.org>
2024-04-25 13:28:16 +0300

    add components

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -7,6 +7,8 @@ import {
   GetRoomCardData,
   Container,
   CommentList,
+  RoomFeatures,
+  Typography,
 } from '@/components';
 
 import style from './style.module.scss';
@@ -23,15 +25,27 @@ const RoomPage: FC<Props> = ({ params }) => {
             <div className={style['content-column']}>
               <div className={style['room-statistic']}>
                 <section className={style['icon-text-list']}>
-                  icon-text-list
+                  <RoomFeatures />
                 </section>
                 <section className={style['pie-chart']}>pie-chart</section>
               </div>
               <CommentList />
+              <div className={style.rules}>
+                <section className={style['bullet-list']}>rules</section>
+                <section className={style.reset}>
+                  <Typography tag="h2">Отмена</Typography>
+                  <div className={style.text}>
+                    Бесплатная отмена в течение 48 ч. После этого при отмене не
+                    позднее чем за 5 дн. до прибытия вы получите полный возврат
+                    за вычетом сбора за услуги.
+                  </div>
+                </section>
+              </div>
             </div>
-          </div>
-          <div className={style['room-booking']}>
-            <BookingCard />
+
+            <div className={style['room-booking']}>
+              <BookingCard />
+            </div>
           </div>
         </Container>
       </div>
