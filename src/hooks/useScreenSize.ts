44c3d270ecae5c9bed7c6c1b106eec72ccba32d9
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2024-04-27 15:49:29 +0400

    fix load window size

--- src/hooks/useScreenSize.ts
+++ src/hooks/useScreenSize.ts
@@ -3,12 +3,17 @@
 import { useEffect, useState } from 'react';
 
 const useScreenSize = () => {
-  const [windowSize, setWindowSize] = useState([1080, 720]);
+  const [windowSize, setWindowSize] = useState([0, 0]);
 
   useEffect(() => {
     const windowSizeHandler = () => {
       setWindowSize([window.innerWidth, window.innerHeight]);
     };
+
+    if (windowSize[0] === 0) {
+      windowSizeHandler();
+    }
+
     window.addEventListener('resize', windowSizeHandler);
 
     return () => {
