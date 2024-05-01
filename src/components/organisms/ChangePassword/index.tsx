228b7882349e3375db142027f376392269941667
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-01 15:38:49 +0700

    add validation for form

--- src/components/organisms/ChangePassword/index.tsx
+++ src/components/organisms/ChangePassword/index.tsx
@@ -1,19 +1,41 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 
 import { Button, Input, Modal, Typography } from '@/components/atoms';
 
 import style from './style.module.scss';
+import { FormErrors, formScheme } from './sheme';
 
 const ChangePassword = () => {
+  const errorsRef = useRef<FormErrors>({ _errors: [] });
+  const [errors, setErrors] = useState<FormErrors>({ _errors: [] });
   const [isOpen, setIsOpen] = useState(false);
 
   const handlePasswordFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    // const data = new FormData(e.currentTarget);
-    // const formData = Object.fromEntries(data.entries());
+    const data = new FormData(e.currentTarget);
+    const formData = Object.fromEntries(data.entries());
+    const validResult = formScheme.safeParse(formData);
+
+    if ('error' in validResult) {
+      const validErrors = validResult.error.format();
+      errorsRef.current = validErrors;
+    }
+
+    if (validResult.success) {
+      errorsRef.current = { _errors: [] };
+    }
+
+    if (formData['newPassword'] !== formData['repeatPassword']) {
+      errorsRef.current = {
+        ...errorsRef.current,
+        repeatPassword: { _errors: ['Пароль не совпадает с введённым выше'] },
+      };
+    }
+
+    setErrors(errorsRef.current);
   };
 
   return (
@@ -30,18 +52,22 @@ const ChangePassword = () => {
           <Input
             placeholder="Старый пароль"
             type="password"
-            name="oldPassrord"
+            name="oldPassword"
+            error={errors.oldPassword ? errors.oldPassword._errors[0] : ''}
           />
           <Input
             placeholder="Новый пароль"
             type="password"
-            name="newPassrord"
+            name="newPassword"
+            error={errors.newPassword ? errors.newPassword._errors[0] : ''}
           />
           <Input
             placeholder="Повторите новый пароль"
             type="password"
-            name="repeatPassrord"
-            error="asdadsads"
+            name="repeatPassword"
+            error={
+              errors.repeatPassword ? errors.repeatPassword._errors[0] : ''
+            }
           />
           <div className={style.change_button}>
             <Button
--- src/components/organisms/ChangePassword/sheme.ts
+++ src/components/organisms/ChangePassword/sheme.ts
@@ -1,9 +1,28 @@
-import { z } from 'zod';
+import { ZodFormattedError, z } from 'zod';
 
 const formScheme = z.object({
-  oldPassrord: z.string(),
-  newPassrord: z.string(),
-  repeatPassword: z.string(),
+  oldPassword: z
+    .string()
+    .min(1, { message: 'Это поле обязательно для заполнения' })
+    .min(8, { message: 'Минимальная длина пароля 8 символов' }),
+  newPassword: z
+    .string()
+    .min(1, { message: 'Это поле обязательно для заполнения' })
+    .min(8, { message: 'Минимальная длина пароля 8 символов' }),
+  repeatPassword: z
+    .string()
+    .min(1, { message: 'Это поле обязательно для заполнения' })
+    .min(8, { message: 'Минимальная длина пароля 8 символов' }),
 });
 
+type FormErrors = ZodFormattedError<
+  {
+    oldPassword: string;
+    newPassword: string;
+    repeatPassword: string;
+  },
+  string
+>;
+
 export { formScheme };
+export type { FormErrors };
