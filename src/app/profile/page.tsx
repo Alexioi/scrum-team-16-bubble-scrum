d3106fd23fdf17726795c5e513472cf5fe795da5
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-05-01 14:15:54 +0700

    add modal window

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,15 +4,31 @@ import { redirect } from 'next/navigation';
 
 import { selectUID } from '@/store';
 import { useAppSelector } from '@/hooks';
+import { Modal } from '@/components';
+import { useState } from 'react';
 
 const Profile = () => {
+  const [open, setOpen] = useState(false);
+
   const uid = useAppSelector(selectUID);
 
   if (uid === null) {
     return redirect('/');
   }
 
-  return null;
+  return (
+    <>
+      <button onClick={() => setOpen(true)} />
+      <Modal
+        isOpen={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
+        <div style={{ height: '2000px' }} />
+      </Modal>
+    </>
+  );
 };
 
 export default Profile;
