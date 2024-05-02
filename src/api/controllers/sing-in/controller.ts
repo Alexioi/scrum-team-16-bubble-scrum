7c60a3c976ab51bc12b0cbeb31be7f49b9d0c351
Ivanov Alexey <ivanovdev@disroot.org>
2024-05-02 09:37:26 +0300

    separete api functions

--- src/api/controllers/sing-in/controller.ts
+++ src/api/controllers/sing-in/controller.ts
@@ -1,39 +1,9 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { collection, query, where, getDocs } from 'firebase/firestore';
 
-import { INCORRECT_DATA_ERROR } from '@/constants';
-import { userInfoScheme } from '@/schemes';
-
-import { auth, db } from '../../initFirebase';
-
-const getUserInfo = async () => {
-  const user = auth.currentUser;
-
-  if (user === null || user.email === null) {
-    return null;
-  }
-
-  const usersInfoCollection = collection(db, 'users-info');
-
-  const q = query(usersInfoCollection, where('uid', '==', user.uid));
-
-  const querySnapshot = await getDocs(q);
-
-  const result = userInfoScheme.safeParse(
-    querySnapshot.docs.map((el) => {
-      return { ...el.data() };
-    })[0],
-  );
-
-  if (!result.success) {
-    throw new Error(INCORRECT_DATA_ERROR);
-  }
-
-  return { uid: user.uid, email: user.email, ...result.data };
-};
+import { auth } from '../../initFirebase';
 
 const login = async (email: string, password: string) => {
   await signInWithEmailAndPassword(auth, email, password);
 };
 
-export { getUserInfo, login };
+export { login };
